90868d14d2f4544453c42bbf8c432680aa073f2f
Asaf Axelrod <AxelrodAsaf@gmail.com>
2026-02-26 00:25:18 +0200

    sync: update canonical dwm_config.h (Mod4, notes integration, custom keybindings)

--- configs/dwm_config.h
+++ configs/dwm_config.h
@@ -46,7 +46,7 @@ static const Layout layouts[] = {
 };
 
 /* key definitions */
-#define MODKEY Mod1Mask
+#define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -61,10 +61,14 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
 
+static const char *notescmd[] = { "notes", NULL };
+static const char *help_cmd[] = { "dwm-help", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,		XK_n,	   spawn,     {.v = notescmd } },
+	{ MODKEY|ShiftMask,             XK_question,  spawn,	   {.v = help_cmd } },
+	{ MODKEY,                       XK_p,      spawn,     SHCMD("~/.local/bin/dmenu-runapps") },
+	{ MODKEY|ShiftMask,   		XK_p,      spawn,     SHCMD("~/.local/bin/dmenu-killproc") },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -95,7 +99,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,             XK_x,      quit,           {0} },
 };
 
 /* button definitions */
